f28f59c35bb7566d072cdb569a34828856f44d57
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-05 18:18:11 +0300

    create product handler added using mediatr

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    // Why record?
+    // A record is a reference type that provides built-in functionality for encapsulating data.
+
+    // This is the command to create a product, it allows us to encapsulate all the data needed to create a product in a single object.
+    // This is useful to pass the data to the command handler and to validate the data before creating the product.
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+
+    // This is the result of the command, it contains the ID of the created product.
+    // This is useful to return the ID of the product to the client after it has been created.
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
