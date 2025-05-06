9db57f7b1ef1ca3b145c7430e7b750d2096b3675
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-06 11:46:52 +0300

    Catalog.API: developed post endpoint for create product using carter, mediatr and mapster

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,34 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            // We get the request from the body of the HTTP POST request and map it to the CreateProductRequest object.
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                // We use Mapster to map the request to the command object.
+                var command = request.Adapt<CreateProductCommand>();
+
+                // We use MediatR to send the command to the command handler.
+                var result = await sender.Send(command);
+
+                // We use Mapster to map the result to the response object.
+                var response = result.Adapt<CreateProductResponse>();
+
+                // We return the response object with a 201 Created status code and the location of the created product.
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create a new product")
+            .WithDescription("Create a new product");
+
+        }
     }
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
@@ -8,17 +9,33 @@ namespace Catalog.API.Products.CreateProduct
     // This is the command to create a product, it allows us to encapsulate all the data needed to create a product in a single object.
     // This is useful to pass the data to the command handler and to validate the data before creating the product.
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-        : IRequest<CreateProductResult>;
+        : ICommand<CreateProductResult>;
 
     // This is the result of the command, it contains the ID of the created product.
     // This is useful to return the ID of the product to the client after it has been created.
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // create a new product with the data from the command
+            // save to database
+            // return CreateProductResult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // TODO
+            // save the product to the database
+
+            return new CreateProductResult(Guid.NewGuid()); // Simulate product creation and return a new ID.
         }
     }
 
