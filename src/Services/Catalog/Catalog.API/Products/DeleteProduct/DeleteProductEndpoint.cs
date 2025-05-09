16b0ddf2af48a8e4b092b585a0d65e26ba28b047
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-09 16:20:35 +0300

    Catalog.API: DeleteProductEndpoint developed

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    // public record DeleteProductRequest(Guid Id); // this request input should match with the DeleteProductCommand to map correctly
+
+    public record DeleteProductResponse(bool IsSuccess);
+
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id)); // MediatR
+
+                var response = result.Adapt<DeleteProductResponse>(); // Mapster
+
+                return Results.Ok(response); // return the response object
+            })
+                .WithName("DeleteProduct")
+                .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Delete a product")
+                .WithDescription("Delete a product");
+        }
+    }
+}
