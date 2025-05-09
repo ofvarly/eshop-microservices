b90b72a28403e2ba2642765e8707653d6ab584ca
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-09 17:52:03 +0300

    FluentValidation added
    FluentValidation added to BuildingBlocks and implemented to Catalog.API

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -12,16 +12,35 @@
     // This is useful to return the ID of the product to the client after it has been created.
     public record CreateProductResult(Guid Id);
 
+
+    // This is the request object that is used to map the data from the HTTP POST request to the command object.
+    // It is used to validate the data before it is passed to the command handler. (Middleware)
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            // FluentValidation is a library for building strongly-typed validation rules.
+            // It is used to validate the data in the command before it is passed to the command handler.
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+        }
+    }
+
     // IDocumentSession is a Marten interface that represents a session with the database.
     // It is used to perform CRUD operations on the database.
     // Why not use a repository pattern?
     // The repository pattern is an abstraction that provides a way to access data from a data source.
     // It is used to decouple the data access logic from the business logic.
     // The reason we don't use a repository pattern is that Marten provides a built-in way to access data from the database.
-    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
             // create a new product with the data from the command
             // save to database
             // return CreateProductResult result
