6c05abce26c64b462669ddd573f7fd9976a546c0
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-12 14:41:11 +0300

    Catalog.API: ExceptionHandler converted to CustomExceptionHandler in Program.cs

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -23,6 +23,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapCarter();
 
-app.UseExceptionHandler(options => { });
+app.UseExceptionHandler(options => {});
 
 app.Run();
