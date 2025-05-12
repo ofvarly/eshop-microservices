848904efe825fbd2c4c4c681cf69d1a9110da50a
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-12 16:50:26 +0300

    Catalog.API: Pagination added to GetProducts

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,15 +1,19 @@
 
 namespace Catalog.API.Products.GetProducts
 {
+    public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10); // this request input should match with the GetProductsQuery to map correctly
     public record GetProductsResponse(IEnumerable<Product> Products); // this response output should match with the GetProductsResult to map correctly
 
     public class GetProductsEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products", async (ISender sender) =>
+            // [AsParameters] is used to bind the query parameters to the request object
+            app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
             {
-                var result = await sender.Send(new GetProductsQuery()); // MediatR
+                var query = request.Adapt<GetProductsQuery>(); // Mapster
+
+                var result = await sender.Send(query); // MediatR
 
                 var response = result.Adapt<GetProductsResponse>(); // Mapster
 
