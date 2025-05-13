a71748a167778e05104b0994d16c8ef3df8c87eb
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-13 17:42:02 +0300

    Basket.API: MediatR and Carter added to Program.cs

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
 
 // Add services to the container.
+
+builder.Services.AddCarter(); // Add Carter for API routing
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddValidatorsFromAssembly(assembly); // Add FluentValidation for request validation
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.MapCarter(); // Map Carter routes
 
 app.Run();
