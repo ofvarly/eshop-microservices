6c8ffe1933141c8ed546ec59724c1640c7439491
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-13 16:13:45 +0300

    Basket.API: GetBasketEndpoint developed

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    // public record GetBasketRequest(string UserName); // this request input should match with the GetBasketResult to map correctly
+    public record GetBasketResponse(ShoppingCart Cart);
+
+    public class GetBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{username}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(userName)); // MediatR
+
+                var response = result.Adapt<GetBasketResponse>(); // Mapster
+
+                return Results.Ok(response);
+            })
+                .WithName("GetBasket")
+                .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get a basket by username")
+                .WithDescription("Get a basket by username");
+        }   
+    }
+}
