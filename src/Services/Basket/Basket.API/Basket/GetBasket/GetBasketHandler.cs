c84141efa0c3c5720f6103c97a7a10bba68a0b91
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-13 15:50:41 +0300

    GetBasketHandler developed

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //var basket = await _basketRepository.GetBasket(query.UserName, cancellationToken);
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
+    }
+    {
+
+    }
+}
