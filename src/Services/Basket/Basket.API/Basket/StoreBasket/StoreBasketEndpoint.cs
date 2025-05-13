0951cdd9f0c514a343f379fa2d4393ee5bd71787
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-13 17:02:46 +0300

    Basket.API: StoreBasketEndpoint developed

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketRequest(ShoppingCart Cart);
+    public record StoreBasketResponse(string UserName);
+
+    public class StoreBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>(); // Mapster
+
+                var result = await sender.Send(command); // MediatR
+                
+                var response = result.Adapt<StoreBasketResponse>(); // Mapster
+
+                return Results.Created($"/basket/{response.UserName}", response);
+            })
+                .WithName("StoreBasket")
+                .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Store a shopping cart")
+                .WithDescription("Store a shopping cart");
+        }
+    }
+}
