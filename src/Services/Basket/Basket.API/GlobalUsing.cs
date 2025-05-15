5b7a52b8ffa60e9377c81e7ee3b5fc7ae1fb61f8
Ömer Faruk Varlı <omerfaruk_varli21@erdogan.edu.tr>
2025-05-15 11:26:56 +0300

    Basket.API: Repositories added

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,8 +1,10 @@
 ﻿global using Basket.API.Models;
+global using Basket.API.Exceptions;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
 global using Carter;
 global using MediatR;
 global using Mapster;
 global using FluentValidation;
-global using BuildingBlocks.Behaviors;
+global using Marten;
 
